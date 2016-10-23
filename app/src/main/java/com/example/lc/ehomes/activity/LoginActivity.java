cf1333cd81266273e0dbf01edc4759341ffe5f16
LuChengLC <14clu@stu.edu.cn>
2016-10-23 22:26:20 +0800

    2. 初次报错解决

--- app/src/main/java/com/example/lc/ehomes/activity/LoginActivity.java
+++ app/src/main/java/com/example/lc/ehomes/activity/LoginActivity.java
@@ -4,27 +4,18 @@ package com.example.lc.ehomes.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.lc.ehomes.R;
 
-import butterknife.BindView;
 
 public class LoginActivity extends AppCompatActivity {
-    Boolean flag_MsgTrue = false;
-
-    @BindView(R.id.login_button)
-    Button login_button;
-    @BindView(R.id.register_button)
-    Button register_button;
-    @BindView(R.id.count_edit)
-    EditText count_edit;
-    @BindView(R.id.password_edit)
-    EditText password_edit;
-
 
 
     @Override
@@ -33,9 +24,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+        EditText password_edit = (EditText)findViewById(R.id.password_edit);
+        EditText count_edit = (EditText)findViewById(R.id.count_edit);
+        Button login_buton = (Button) findViewById(R.id.login_button);
+        Button register_button = (Button) findViewById(R.id.register_button);
+
+        login_buton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "正在输入", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
+
 
 
 }
